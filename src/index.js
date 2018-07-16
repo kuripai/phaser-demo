a7ca93e2128c8fe206c0d420756763dc0d627873
rainkinz <brendan.grainger@gmail.com>
2018-07-16 14:38:34 -0400

    fixed merge conflict

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-const Phaser = require("phaser");
-
-var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 200 }
-      }
-  },
-  scene: {
-      preload: preload,
-      create: create
-  }
-};
-
-var game = Phaser.Game(config);
-
-function preload() {
-  console.log("preloading");
-}
-
-function create() {
-  console.log("creating");
-=======
 import "phaser"
 var config = {
     type: Phaser.AUTO,
@@ -53,20 +25,19 @@ function preload ()
 
 function create ()
 {
-    //this.add.image("300", "300", "logo"); 
+    //this.add.image("300", "300", "logo");
     const particles = this.add.particles("red");
     var emitter = particles.createEmitter(
         {
-          speed: 100, 
+          speed: 100,
           scale: { start: 1, end: 0 },
           blendMode: 'ADD'
         }
-        
+
     )
     var logo = this.physics.add.image(300, 300, 'logo');
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
     emitter.startFollow(logo);
->>>>>>> d5e26fc2799faae5e845d38dce52bc2b4bbcb4b8
 }
