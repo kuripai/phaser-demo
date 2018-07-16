5d3084a8114f2a61d26a72e8f59a609dc8138f20
rainkinz <brendan.grainger@gmail.com>
2018-07-16 15:44:36 -0400

    fixed initialization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import "phaser"
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game({
+  width: 800, 
+  height: 600, 
+  type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+});
 
 function preload() {
-
-    game.load.atlas('breakout', 'assets/games/breakout/breakout.png', 'assets/games/breakout/breakout.json');
-    game.load.image('starfield', 'assets/misc/starfield.jpg');
-
+    this.load.atlas('breakout', 'assets/games/breakout/breakout.png', 'assets/games/breakout/breakout.json');
+    this.load.image('starfield', 'assets/misc/starfield.jpg');
 }
 
 var ball;
@@ -22,18 +35,16 @@ var scoreText;
 var livesText;
 var introText;
 
-var s;
-
 function create() {
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    // this.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  We check bounds collisions against all walls other than the bottom one
-    game.physics.arcade.checkCollision.down = false;
+    // this.physics.arcade.checkCollision.down = false;
 
-    s = game.add.tileSprite(0, 0, 800, 600, 'starfield');
+    var s = this.add.tileSprite(0, 0, 800, 600, 'starfield');
 
-    bricks = game.add.group();
+    bricks = this.add.group();
     bricks.enableBody = true;
     bricks.physicsBodyType = Phaser.Physics.ARCADE;
 
@@ -49,20 +60,20 @@ function create() {
         }
     }
 
-    paddle = game.add.sprite(game.world.centerX, 500, 'breakout', 'paddle_big.png');
+    paddle = this.add.sprite(this.world.centerX, 500, 'breakout', 'paddle_big.png');
     paddle.anchor.setTo(0.5, 0.5);
 
-    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    this.physics.enable(paddle, Phaser.Physics.ARCADE);
 
     paddle.body.collideWorldBounds = true;
     paddle.body.bounce.set(1);
     paddle.body.immovable = true;
 
-    ball = game.add.sprite(game.world.centerX, paddle.y - 16, 'breakout', 'ball_1.png');
+    ball = this.add.sprite(this.world.centerX, paddle.y - 16, 'breakout', 'ball_1.png');
     ball.anchor.set(0.5);
     ball.checkWorldBounds = true;
 
-    game.physics.enable(ball, Phaser.Physics.ARCADE);
+    this.physics.enable(ball, Phaser.Physics.ARCADE);
 
     ball.body.collideWorldBounds = true;
     ball.body.bounce.set(1);
@@ -71,12 +82,12 @@ function create() {
 
     ball.events.onOutOfBounds.add(ballLost, this);
 
-    scoreText = game.add.text(32, 550, 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
-    livesText = game.add.text(680, 550, 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
-    introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
+    scoreText = this.add.text(32, 550, 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
+    livesText = this.add.text(680, 550, 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
+    introText = this.add.text(this.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
     introText.anchor.setTo(0.5, 0.5);
 
-    game.input.onDown.add(releaseBall, this);
+    this.input.onDown.add(releaseBall, this);
 
 }
 
